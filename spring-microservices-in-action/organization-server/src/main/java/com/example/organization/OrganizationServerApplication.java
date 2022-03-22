d0aa80f4affd191ff933d9c2b0d4ee5770efc11d
wangxi <wangxi009567@sina.com>
2022-03-22 23:00:03 +0800

    Add: Adding new chapter Eureka server & client

---
+++ spring-microservices-in-action/organization-server/src/main/java/com/example/organization/OrganizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.organization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrganizationServerApplication.class, args);
+    }
+
+}
