b4dc7379b4dbc0cbbc5a999d152973cc75fbf9a4
wangxi <wangxi009567@sina.com>
2022-04-12 00:00:22 +0800

    Add: Hystrix in pom.xml & @EnableHystrix in starter method

--- spring-microservices-in-action/licensing-service/src/main/java/com/action/licensing/LicensingServiceApplication.java
+++ spring-microservices-in-action/licensing-service/src/main/java/com/action/licensing/LicensingServiceApplication.java
@@ -3,8 +3,10 @@ package com.action.licensing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableHystrix // Tells Spring Cloud you’re going to use Hystrix for your service
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
